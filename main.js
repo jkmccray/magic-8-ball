28ca837ccc51f851cda78a3338e995d7656c6ea9
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-05 12:25:51 -0500

    Refactored functions to improve code quality and make fixes.

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
+const questionInput = document.getElementById("question");
+const answerOutput = document.getElementById("answer");
+const btn = document.getElementById("btn");
+
 //function that will give an answer to the question/tell a fortune
 function newFortune() {
-  let newFort = randomAnswer(fortunes);
-  let btn = document.getElementById("btn");
-  let btnText = btn.textContent;
-  injectHTML(newFort, "answer");
-  btnText === "My Answer" ? btnText = "Ask Again" : btnText = "My Answer";
-  console.log(btnText);
-  document.getElementById("question").value = "";
+  if (readyForFortune()) {
+    let newFort = randomAnswer(fortunes);
+    injectHTML(newFort, "answer");
+  }
+}
+
+function readyForFortune() {
+  if (fortuneNotYetProvided() && !inputIsEmpty()) {
+    btn.innerHTML = "Ask Again";
+    return true; // get a response from app
+  } else {
+    btn.innerHTML = "My Answer";
+    clearText();
+    return false; // don't get a response from app
+  }
+}
+
+function fortuneNotYetProvided() {
+  return btn.textContent === "My Answer"; // returns true or false
+}
+
+function inputIsEmpty() {
+  if (questionInput.value === '') {
+    alert("Please ask a question");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function clearText() {
+  questionInput.value = "";
+  answerOutput.textContent = "";
 }
 
 //function to product random number from 0 to the length of the array (minus 1 bc of Math.floor)
